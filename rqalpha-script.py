77ef1c5b6d0ebc2042a42eee17a04d80bbf7cdbc
芦厚祥 <houxiang.lu@inin88.com>
2017-06-27 16:38:20 +0800

    Feat: 更新到版本3.0.1

--- rqalpha-script.py
+++ rqalpha-script.py
@@ -1,6 +1,6 @@
 #!"C:\Program Files\Anaconda3\envs\python3.5.3\python.exe"
-# EASY-INSTALL-ENTRY-SCRIPT: 'rqalpha==2.2.7','console_scripts','rqalpha'
-__requires__ = 'rqalpha==2.2.7'
+# EASY-INSTALL-ENTRY-SCRIPT: 'rqalpha==3.0.1','console_scripts','rqalpha'
+__requires__ = 'rqalpha==3.0.1'
 import re
 import sys
 from pkg_resources import load_entry_point
@@ -8,5 +8,5 @@ from pkg_resources import load_entry_point
 if __name__ == '__main__':
     sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
     sys.exit(
-        load_entry_point('rqalpha==2.2.7', 'console_scripts', 'rqalpha')()
+        load_entry_point('rqalpha==3.0.1', 'console_scripts', 'rqalpha')()
     )
