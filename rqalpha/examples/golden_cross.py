12e936905f1b944d63ef9d5bf4a5f39fdadf93fd
芦厚祥 <houxiang.lu@inin88.com>
2017-08-22 22:10:39 +0800

    Feat: 调试变量参数

--- rqalpha/examples/golden_cross.py
+++ rqalpha/examples/golden_cross.py
@@ -8,8 +8,8 @@ def init(context):
     context.s1 = "000001.XSHE"
 
     # 设置这个策略当中会用到的参数，在策略中可以随时调用，这个策略使用长短均线，我们在这里设定长线和短线的区间，在调试寻找最佳区间的时候只需要在这里进行数值改动
-    context.SHORTPERIOD = 20
-    context.LONGPERIOD = 120
+    context.SHORTPERIOD = 5
+    context.LONGPERIOD = 10
 
 
 # 你选择的证券的数据更新将会触发此段逻辑，例如日或分钟历史数据切片或者是实时数据切片更新
