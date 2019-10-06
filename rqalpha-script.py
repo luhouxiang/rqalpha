0176f6dbb03dc761b066d4d8d054bb3a25301898
luhouxiang <luhouxiang@hotmail.com>
2019-10-06 18:47:33 +0800

    Feat: 运行setup，将之更新到3.4.1

--- rqalpha-script.py
+++ rqalpha-script.py
@@ -1,6 +1,6 @@
-#!"C:\Program Files\Anaconda3\envs\python3.6\python.exe"
-# EASY-INSTALL-ENTRY-SCRIPT: 'rqalpha==3.0.9','console_scripts','rqalpha'
-__requires__ = 'rqalpha==3.0.9'
+#!C:\ProgramData\Anaconda3\envs\python3.6.5\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'rqalpha==3.4.1','console_scripts','rqalpha'
+__requires__ = 'rqalpha==3.4.1'
 import re
 import sys
 from pkg_resources import load_entry_point
@@ -8,5 +8,5 @@ from pkg_resources import load_entry_point
 if __name__ == '__main__':
     sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
     sys.exit(
-        load_entry_point('rqalpha==3.0.9', 'console_scripts', 'rqalpha')()
+        load_entry_point('rqalpha==3.4.1', 'console_scripts', 'rqalpha')()
     )
